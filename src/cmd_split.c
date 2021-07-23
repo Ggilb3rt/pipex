780daa148874e6147e9176587c952224a850b304
Guillaume GILBERT <ggilbert@e2r8p7.42.fr>
2021-07-23 20:50:35 +0200

    segfault with space, trying to solve in cmd_split but doesn't works

--- src/cmd_split.c
+++ src/cmd_split.c
@@ -6,7 +6,7 @@
 /*   By: ggilbert <ggilbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/16 13:26:20 by ggilbert          #+#    #+#             */
-/*   Updated: 2021/07/16 13:27:44 by ggilbert         ###   ########.fr       */
+/*   Updated: 2021/07/23 19:38:25 by ggilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,9 @@ t_bool	split_cmds(char **av, char separator, t_cmds *cmds)
 		cmds->cmd1 = ft_split(av[2], separator);
 		cmds->cmd2 = ft_split(av[3], separator);
 	}
+	if (cmds->cmd1 == NULL)
+		cmds->cmd1 = put_empty();
+	if (cmds->cmd2 == NULL)
+		cmds->cmd2 = put_empty();
 	return (1);
 }
