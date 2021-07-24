9adfdb019acad25b7b6c68a0d3dbf8c5ac7ba4ce
Guillaume GILBERT <ggilbert@e1r10p23.42.fr>
2021-07-24 10:54:29 +0200

    leaks correction, remove leaks and put null checker out of main function

--- src/cmd_split.c
+++ src/cmd_split.c
@@ -6,7 +6,7 @@
 /*   By: ggilbert <ggilbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/16 13:26:20 by ggilbert          #+#    #+#             */
-/*   Updated: 2021/07/24 10:17:59 by ggilbert         ###   ########.fr       */
+/*   Updated: 2021/07/24 10:51:53 by ggilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,22 @@ char	**put_empty(void)
 	return (ret);
 }
 
+void	change_null_to_empty(t_cmds *cmds)
+{
+	if (cmds->cmd1[0] == NULL)
+	{
+		if (cmds->cmd1)
+			free(cmds->cmd1);
+		cmds->cmd1 = put_empty();
+	}
+	if (cmds->cmd2[0] == NULL)
+	{
+		if (cmds->cmd2)
+			free(cmds->cmd2);
+		cmds->cmd2 = put_empty();
+	}
+}
+
 t_bool	split_cmds(char **av, char separator, t_cmds *cmds)
 {
 	if ((av[2] && !av[2][0]) && (av[3] && !av[3][0]))
@@ -33,17 +49,14 @@ t_bool	split_cmds(char **av, char separator, t_cmds *cmds)
 	}
 	else if (av[3] && !av[3][0])
 	{
-		cmds->cmd2 = put_empty();
 		cmds->cmd1 = ft_split(av[2], separator);
+		cmds->cmd2 = put_empty();
 	}
 	else
 	{
 		cmds->cmd1 = ft_split(av[2], separator);
 		cmds->cmd2 = ft_split(av[3], separator);
 	}
-	if (cmds->cmd1[0] == NULL)
-		cmds->cmd1 = put_empty();
-	if (cmds->cmd2[0] == NULL)
-		cmds->cmd2 = put_empty();
+	change_null_to_empty(cmds);
 	return (1);
 }
